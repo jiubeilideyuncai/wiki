2c8090b61f6623b839a3d54504dd138fa49bb560
kongxy <kongxy5518@163.com>
2025-04-09 20:00:29 +0800

    使用AOP打印接口耗时（也可用Filter或interceptor实现）

---
+++ src/main/java/com/jiawa/wiki/config/SpringMvcConfig.java
@@ -0,0 +1,19 @@
+package com.jiawa.wiki.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+//    @Resource
+//    LogInterceptor logInterceptor;
+//
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(logInterceptor)
+//                .addPathPatterns("/**")
+//                .excludePathPatterns(
+//                        "login"
+//                );
+//    }
+}
