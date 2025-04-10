26acbedc061489e6c495c061ab9e026310ba9518
chuxiaohong <kongxy5518@163.com>
2025-04-11 04:29:44 +0800

    Revert "Merge pull request #5 from jiubeilideyuncai/ui"
    This reverts commit 56a86ad1447d9d943778cda3e238bee0045ccc19, reversing
    changes made to b63214c4754b6f4f405845cbfc04053534fce338.

--- src/main/java/com/jiawa/wiki/WikiApplication.java
+++ src/main/java/com/jiawa/wiki/WikiApplication.java
@@ -1,5 +1,6 @@
 package com.jiawa.wiki;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.env.Environment;
@@ -7,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 
 @SpringBootApplication
+@MapperScan("com.jiawa.wiki.mapper")
 public class WikiApplication {
     private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
 
--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,27 +1,19 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import HomeView from '../views/homeView.vue'
-import AboutView from "@/views/aboutView.vue";
-import AdminEbook from "@/views/admin/admin-ebook.vue";
+import HomeView from '../views/HomeView.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    name: 'Home',
+    name: 'home',
     component: HomeView
   },
   {
     path: '/about',
-    name: 'About',
-    component:AboutView,
+    name: 'about',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    // component: () => import(/* webpackChunkName: "about" */ '../views/aboutView.vue')
-  },
-  {
-    path: '/admin/ebook',
-    name: 'AdminEbook',
-    component: AdminEbook
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
   }
 ]
 
