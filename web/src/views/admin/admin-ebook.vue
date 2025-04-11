d2ffce4c951c7b2b704e2f7b39a456efd752dec8
chuxiaohong <2218127600@qq.com>
2025-04-11 17:33:37 +0800

    电子书管理页面增加list及分页

---
+++ web/src/views/admin/admin-ebook.vue
@@ -0,0 +1,253 @@
+<template>
+  <a-layout>
+    <a-layout-content
+        :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+    >
+      <a-table
+          :columns="columns"
+          :row-key="record => record.id"
+          :data-source="ebooks"
+          :pagination="pagination"
+          :loading="loading"
+          @change="handleTableChange"
+      >
+        <template #cover="{ text: cover }">
+          <img v-if="cover" :src="cover" alt="avatar"/>
+        </template>
+        <template v-slot:action="{ text, record }">
+          <a-space size="small">
+            <a-button type="primary">
+              编辑
+            </a-button>
+            <a-button type="primary" danger>
+              删除
+            </a-button>
+          </a-space>
+        </template>
+      </a-table>
+    </a-layout-content>
+  </a-layout>
+</template>
+<script lang="ts">
+import {defineComponent, onMounted, ref} from 'vue';
+import axios from 'axios';
+
+export default defineComponent({
+  name: 'AdminEbook',
+  setup() {
+    const ebooks = ref();
+    const pagination = ref({
+      current: 1,
+      pageSize: 4,
+      total: 0
+    });
+    const loading = ref(false);
+
+    const columns = [
+      {
+        title: '封面',
+        dataIndex: 'cover',
+        slots: {customRender: 'cover'}
+      },
+      {
+        title: '名称',
+        dataIndex: 'name'
+      },
+      {
+        title: '分类一',
+        key: 'category1Id',
+        dataIndex: 'category1Id'
+      },
+      {
+        title: '分类二',
+        dataIndex: 'category2Id'
+      },
+      {
+        title: '文档数',
+        dataIndex: 'docCount'
+      },
+      {
+        title: '阅读数',
+        dataIndex: 'viewCount'
+      },
+      {
+        title: '点赞数',
+        dataIndex: 'voteCount'
+      },
+      {
+        title: 'Action',
+        key: 'action',
+        slots: {customRender: 'action'}
+      }
+    ];
+
+    /**
+     * 数据查询
+     **/
+    const handleQuery = (params: any) => {
+      loading.value = true;
+      // 如果不清空现有数据，则编辑保存重新加载数据后，再点编辑，则列表显示的还是编辑前的数据
+      ebooks.value = [];
+      axios.get("/ebook/list",{
+        params:{
+          page: params.page,
+          size: params.size
+        }
+      }).then((response) => {
+        console.log("数据查询");
+        loading.value = false;
+        const data = response.data;
+        ebooks.value = data.content.list;
+
+        // 重置分页按钮
+        pagination.value.current = params.page;
+        pagination.value.total = data.content.total;
+      });
+    };
+
+    /**
+     * 表格点击页码时触发
+     */
+    const handleTableChange = (pagination: any) => {
+      console.log("看看自带的分页参数都有啥：" + pagination);
+      handleQuery({
+        page: pagination.current,
+        size: pagination.pageSize
+      });
+    };
+
+    onMounted(() => {
+      handleQuery({
+        page:1,
+        size:pagination.value.pageSize
+      });
+    });
+
+    return {
+      ebooks,
+      pagination,
+      columns,
+      loading,
+      handleTableChange
+    }
+  }
+});
+</script>
+
+
+<!--<script lang="ts">-->
+<!--import { defineComponent, onMounted, ref } from 'vue';-->
+<!--import axios from 'axios';-->
+<!--import { message } from 'ant-design-vue';-->
+
+<!--export default defineComponent({-->
+<!--  name: 'AdminEbook',-->
+<!--  setup() {-->
+<!--    const param = ref();-->
+<!--    param.value = {};-->
+<!--    const ebooks = ref([]);-->
+<!--    const pagination = ref({-->
+<!--      current: 1,-->
+<!--      pageSize: 10,-->
+<!--      total: 0-->
+<!--    });-->
+<!--    const loading = ref(false);-->
+
+<!--    const columns = [-->
+<!--      {-->
+<!--        title: '封面',-->
+<!--        dataIndex: 'cover',-->
+<!--        slots: { customRender: 'cover' }-->
+<!--      },-->
+<!--      {-->
+<!--        title: '名称',-->
+<!--        dataIndex: 'name'-->
+<!--      },-->
+<!--      {-->
+<!--        title: '分类',-->
+<!--        slots: { customRender: 'category' }-->
+<!--      },-->
+<!--      {-->
+<!--        title: '文档数',-->
+<!--        dataIndex: 'docCount'-->
+<!--      },-->
+<!--      {-->
+<!--        title: '阅读数',-->
+<!--        dataIndex: 'viewCount'-->
+<!--      },-->
+<!--      {-->
+<!--        title: '点赞数',-->
+<!--        dataIndex: 'voteCount'-->
+<!--      },-->
+<!--      {-->
+<!--        title: 'Action',-->
+<!--        key: 'action',-->
+<!--        slots: { customRender: 'action' }-->
+<!--      }-->
+<!--    ];-->
+
+<!--    /**-->
+<!--     * 数据查询-->
+<!--     **/-->
+<!--    const handleQuery = (params: any) => {-->
+<!--      loading.value = true;-->
+<!--      // 如果不清空现有数据，则编辑保存重新加载数据后，再点编辑，则列表显示的还是编辑前的数据-->
+<!--      ebooks.value = [];-->
+<!--      axios.get("/ebook/list", {-->
+<!--        params: {-->
+<!--          page: params.page,-->
+<!--          size: params.size,-->
+<!--          name: param.value.name-->
+<!--        }-->
+<!--      }).then((response) => {-->
+<!--        loading.value = false;-->
+<!--        const data = response.data;-->
+<!--        if (data.success) {-->
+<!--          ebooks.value = data.content.list;-->
+
+<!--          // 重置分页按钮-->
+<!--          pagination.value.current = params.page;-->
+<!--          pagination.value.total = data.content.total;-->
+<!--        } else {-->
+<!--          message.error(data.message);-->
+<!--        }-->
+<!--      });-->
+<!--    };-->
+
+<!--    /**-->
+<!--     * 表格点击页码时触发-->
+<!--     */-->
+<!--    const handleTableChange = (pagination: any) => {-->
+<!--      console.log("看看自带的分页参数都有啥：" + pagination);-->
+<!--      handleQuery({-->
+<!--        page: pagination.current,-->
+<!--        size: pagination.pageSize-->
+<!--      });-->
+<!--    };-->
+
+
+
+
+<!--    onMounted(() => {-->
+<!--      handleQuery({});-->
+<!--    });-->
+
+<!--    return {-->
+<!--      param,-->
+<!--      ebooks,-->
+<!--      pagination,-->
+<!--      columns,-->
+<!--      loading,-->
+<!--      handleTableChange,-->
+<!--      handleQuery-->
+<!--    }-->
+<!--  }-->
+<!--});-->
+<!--</script>-->
+
+<style scoped>
+img {
+  width: 50px;
+  height: 50px;
+}
+</style>
